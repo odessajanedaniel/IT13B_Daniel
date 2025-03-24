e5b98ebd1c517c194f762452eb826217b2b9f879
odessajanedaniel <dessangdaniel@gmail.com>
2025-03-24 11:25:36 +0800

    Add files via upload

---
+++ Daniel_IT13B_Activities/IT13BODESSA DANIEL_LAB1_ACTIVITIES/src/src/Bocon_DanielO_SingleArray.java
@@ -0,0 +1,11 @@
+
+public class Bocon_DanielO_SingleArray {
+    public static void main (String[] args){
+        char letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+        
+        for(char a : letters){
+            System.out.print(a + ", ");
+        }
+    }
+    
+}
